a411ccdba9bbda80205a7338e10467d8e40c3a13
gowthamc0702 <gowthamc684@gmail.com>
2023-12-17 02:52:23 +0530

    database-connection

---
+++ app/(auth)/_components/logo.tsx
@@ -0,0 +1,33 @@
+import Image from "next/image";
+import { twMerge } from "tailwind-merge";
+import { cn } from "@/lib/utils";
+import { Poppins } from "next/font/google";
+
+const font = Poppins({
+  subsets: ["latin"],
+  weight: ["200", "300", "400", "500", "600", "700"],
+});
+
+const Logo = () => {
+  return (
+    <div className=" flex flex-col items-center gap-y-4">
+      <div className="bg-white rounded-full p-2">
+        <Image src="/smile.svg" alt="streamo" height="90" width="90" />
+      </div>
+      <div className={cn(" flex flex-col items-center justify-center ",
+      font.className)}>
+        <p className={cn(
+          'text-xl font-semibold',
+            font.className
+        )}>
+          Streamo
+        </p>
+        <p className="text-sm text-muted-foreground">
+          Game onn
+        </p>
+      </div>
+    </div>
+  );
+}
+ 
+export default Logo;
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,8 @@
+import { ClerkProvider } from '@clerk/nextjs'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import { dark } from '@clerk/themes'
+import { ThemeProvider } from '@/components/theme-provider'
 import './globals.css'
 
 const inter = Inter({ subsets: ['latin'] })
@@ -15,8 +18,16 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
-  )
+    <ClerkProvider appearance={{ baseTheme: dark }}>
+      <html lang="en">
+        <body className={inter.className}>
+          <ThemeProvider
+          attribute='class'
+          forcedTheme='dark'
+          storageKey='streamo-theme'
+          >{children}</ThemeProvider>
+        </body>
+      </html>
+    </ClerkProvider>
+  );
 }
